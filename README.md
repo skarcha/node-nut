ef37bb53aff98e92d7e190df6c4066902e9f6e42
Antonio Pérez <aperez@skarcha.com>
2015-03-08 19:15:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,81 @@
+# Node-NUT
+
+Node-NUT is a NodeJS module that implements a NUT (Network UPS Tools) client.
+
+## Events
+
+### ready
+
+This event is emitted when the connection to the NUT server is established.
+
+### error
+
+Emitted when there was an error establishing the connection or with the communication to
+the server.
+
+### close
+
+Emitted when the connection to the NUT server is closed.
+
+## Functions (tested)
+
+### `start()`
+
+Starts the connection to the server.
+
+### `GetUPSList (callback)`
+
+Calls the `callback` function with the list of UPS as an array as the first parameter.
+
+### `GetUPSVars (upsname, callback)`
+
+Calls the `callback` function with the list of VARs of `upsname`.
+
+### `GetUPSCommands (upsname, callback)`
+
+Calls the `callback` function with the list of COMMANDs available in `upsname`.
+
+### `help(callback)`
+
+Call the `callback` function with `help` as a string.
+
+### `ver (callback)`
+
+Call the `callback` function with the version of the server as a string.
+
+## Functions (not fully tested)
+
+### `GetRWVars (upsname)`
+
+### `SetRWVar (upsname, varname, value)`
+
+### `RunUPSCommand (upsname, command)`
+
+### `FSD (upsname`
+
+### `ListClients (upsname)`
+
+## Example
+
+`var Nut = require('node-nut');
+
+oNut = new Nut(3493, 'localhost');
+
+oNut.on('error', function(err) {
+	console.log('There was an error: ' + err);
+});
+
+oNut.on('close', function() {
+	console.log('Connection closed.');
+});
+
+oNut.on('ready', function() {
+	self = this;
+	this.GetUPSList(function(upslist) {
+		console.log(upslist);
+		self.close();
+	});
+});
+
+oNut.start();`
+
---
+++ node-nut.js
@@ -0,0 +1,152 @@
+var net = require('net');
+var inherits = require('util').inherits;
+var EventEmitter = require('events').EventEmitter;
+
+module.exports = Nut;
+
+function Nut (port, host)
+{
+	EventEmitter.call(this);
+	Nut.init.call(this, port, host);
+}
+
+inherits(Nut, EventEmitter);
+
+Nut.init = function (port, host) {
+	this._port = port;
+	this._host = host;
+}
+
+Nut.prototype.start = function () {
+	var self = this;
+	self.status = 'idle';
+
+	function open () { self.emit('ready'); }
+	function error (err) { self.emit('error', err); }
+	function close () { self.emit('close'); }
+
+	this._client = net.createConnection (self._port, self._host, open);
+	this._client.setEncoding('ascii');
+
+	this._client.on('data', function (data) {
+		if (typeof(self.parseFunc) !== 'undefined') {
+			self.parseFunc(data);
+		}
+		else {
+			self.status = 'idle';
+		}
+	});
+
+	this._client.on('error', error);
+	this._client.on('close', close);
+}
+
+Nut.prototype.send = function (cmd, parseFunc) {
+	if (this.status == 'idle') {
+		this.status = 'waiting';
+		this.parseFunc = parseFunc;
+		this._client.write(cmd + '\n');
+	}
+};
+
+Nut.prototype.close = function () {
+	this.send('LOGOUT');
+}
+
+Nut.prototype.GetUPSList = function (callback) {
+	this.send('LIST UPS', function(data) {
+		var data_array = data.split('\n');
+		var re = /^UPS\s+(.+)\s+"(.+)"/
+		for (i = 0; i < data_array.length-1; i++) {
+			line = data_array[i];
+			if (line.indexOf('BEGIN LIST UPS') == 0) {
+				vars = [];	
+			}
+			else if (line.indexOf('UPS ') == 0) {
+				matches = re.exec(line);
+				vars[matches[1]] = matches[2];
+			}
+			else if (line.indexOf('END LIST UPS') == 0) {
+				this.status = 'idle';
+				callback(vars);
+			}
+		}
+	});
+}
+
+Nut.prototype.GetUPSVars = function (ups, callback) {
+	this.send('LIST VAR ' + ups, function(data) {
+		var data_array = data.split('\n');
+		var re = /^VAR\s+.+\s+(.+)\s+"(.+)"/
+		for (i = 0; i < data_array.length-1; i++) {
+			line = data_array[i];
+			if (line.indexOf('BEGIN LIST VAR') == 0) {
+				vars = [];	
+			}
+			else if (line.indexOf('VAR ' + ups) == 0) {
+				matches = re.exec(line);
+				vars[matches[1]] = matches[2];
+			}
+			else if (line.indexOf('END LIST VAR') == 0) {
+				this.status = 'idle';
+				callback(vars);
+			}
+		}
+	});
+}
+
+Nut.prototype.GetUPSCommands = function (ups, callback) {
+	this.send('LIST CMD ' + ups, function(data) {
+		var data_array = data.split('\n');
+		var re = /^CMD\s+.+\s+(.+)/
+		for (i = 0; i < data_array.length-1; i++) {
+			line = data_array[i];
+			if (line.indexOf('BEGIN LIST CMD') == 0) {
+				commands = [];	
+			}
+			else if (line.indexOf('CMD ') == 0) {
+				matches = re.exec(line);
+				commands.push(matches[1]);
+			}
+			else if (line.indexOf('END LIST CMD') == 0) {
+				this.status = 'idle';
+				callback(commands);
+			}
+		}
+	});
+}
+
+Nut.prototype.GetRWVars = function (ups) {
+	this.send('LIST RW ' + ups);
+}
+
+Nut.prototype.SetRWVar = function (ups, name, value) {
+	this.send('SET VAR ' + ups + ' ' + name + ' ' + value);
+}
+
+Nut.prototype.RunUPSCommand = function (ups, command) {
+	this.send('INSTCMD ' + ups + ' ' + command);
+}
+
+Nut.prototype.FSD = function (ups) {
+	this.send('MASTER ' + ups);
+}
+
+Nut.prototype.help = function (callback) {
+	this.send('HELP', function(data) {
+		this.status = 'idle';
+		callback(data);
+	});
+}
+
+Nut.prototype.ver = function (callback) {
+	this.send('VER', function(data) {
+		this.status = 'idle';
+		callback(data);
+	});
+}
+
+Nut.prototype.ListClients = function (ups) {
+	this.send('LIST CLIENTS ' + ups);
+}
+
