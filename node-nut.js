62fd789f3b69aa2476a3e39af8e881801073bb4c
Ingo Fischer <github@fischer-ka.de>
2017-05-02 13:44:21 +0200

    fix all nslint hints and warnings

--- node-nut.js
+++ node-nut.js
@@ -15,7 +15,7 @@ inherits(Nut, EventEmitter);
 Nut.init = function (port, host) {
 	this._port = port;
 	this._host = host;
-}
+};
 
 Nut.prototype.start = function () {
 	var self = this;
@@ -39,7 +39,7 @@ Nut.prototype.start = function () {
 
 	this._client.on('error', error);
 	this._client.on('close', close);
-}
+};
 
 Nut.prototype.send = function (cmd, parseFunc) {
 	if (this.status == 'idle') {
@@ -51,102 +51,101 @@ Nut.prototype.send = function (cmd, parseFunc) {
 
 Nut.prototype.close = function () {
 	this.send('LOGOUT');
-}
+};
 
 Nut.prototype.GetUPSList = function (callback) {
 	this.send('LIST UPS', function(data) {
 		var data_array = data.split('\n');
-		var re = /^UPS\s+(.+)\s+"(.+)"/
+		var re = /^UPS\s+(.+)\s+"(.+)"/;
 		for (i = 0; i < data_array.length-1; i++) {
 			line = data_array[i];
-			if (line.indexOf('BEGIN LIST UPS') == 0) {
-				vars = [];	
+			if (line.indexOf('BEGIN LIST UPS') === 0) {
+				vars = [];
 			}
-			else if (line.indexOf('UPS ') == 0) {
+			else if (line.indexOf('UPS ') === 0) {
 				matches = re.exec(line);
 				vars[matches[1]] = matches[2];
 			}
-			else if (line.indexOf('END LIST UPS') == 0) {
+			else if (line.indexOf('END LIST UPS') === 0) {
 				this.status = 'idle';
 				callback(vars);
 			}
 		}
 	});
-}
+};
 
 Nut.prototype.GetUPSVars = function (ups, callback) {
 	this.send('LIST VAR ' + ups, function(data) {
 		var data_array = data.split('\n');
-		var re = /^VAR\s+.+\s+(.+)\s+"(.+)"/
+		var re = /^VAR\s+.+\s+(.+)\s+"(.+)"/;
 		for (i = 0; i < data_array.length-1; i++) {
 			line = data_array[i];
-			if (line.indexOf('BEGIN LIST VAR') == 0) {
-				vars = [];	
+			if (line.indexOf('BEGIN LIST VAR') === 0) {
+				vars = [];
 			}
-			else if (line.indexOf('VAR ' + ups) == 0) {
+			else if (line.indexOf('VAR ' + ups) === 0) {
 				matches = re.exec(line);
 				vars[matches[1]] = matches[2];
 			}
-			else if (line.indexOf('END LIST VAR') == 0) {
+			else if (line.indexOf('END LIST VAR') === 0) {
 				this.status = 'idle';
 				callback(vars);
 			}
 		}
 	});
-}
+};
 
 Nut.prototype.GetUPSCommands = function (ups, callback) {
 	this.send('LIST CMD ' + ups, function(data) {
 		var data_array = data.split('\n');
-		var re = /^CMD\s+.+\s+(.+)/
+		var re = /^CMD\s+.+\s+(.+)/;
 		for (i = 0; i < data_array.length-1; i++) {
 			line = data_array[i];
-			if (line.indexOf('BEGIN LIST CMD') == 0) {
-				commands = [];	
+			if (line.indexOf('BEGIN LIST CMD') === 0) {
+				commands = [];
 			}
-			else if (line.indexOf('CMD ') == 0) {
+			else if (line.indexOf('CMD ') === 0) {
 				matches = re.exec(line);
 				commands.push(matches[1]);
 			}
-			else if (line.indexOf('END LIST CMD') == 0) {
+			else if (line.indexOf('END LIST CMD') === 0) {
 				this.status = 'idle';
 				callback(commands);
 			}
 		}
 	});
-}
+};
 
 Nut.prototype.GetRWVars = function (ups) {
 	this.send('LIST RW ' + ups);
-}
+};
 
 Nut.prototype.SetRWVar = function (ups, name, value) {
 	this.send('SET VAR ' + ups + ' ' + name + ' ' + value);
-}
+};
 
 Nut.prototype.RunUPSCommand = function (ups, command) {
 	this.send('INSTCMD ' + ups + ' ' + command);
-}
+};
 
 Nut.prototype.FSD = function (ups) {
 	this.send('MASTER ' + ups);
-}
+};
 
 Nut.prototype.help = function (callback) {
 	this.send('HELP', function(data) {
 		this.status = 'idle';
 		callback(data);
 	});
-}
+};
 
 Nut.prototype.ver = function (callback) {
 	this.send('VER', function(data) {
 		this.status = 'idle';
 		callback(data);
 	});
-}
+};
 
 Nut.prototype.ListClients = function (ups) {
 	this.send('LIST CLIENTS ' + ups);
-}
-
+};
